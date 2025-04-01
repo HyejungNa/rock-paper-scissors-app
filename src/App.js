d0c3ba88c94e56f8862eae7222e2feb135a92dfd
Hyejung Na <hyejung.na01@gmail.com>
2025-04-02 00:40:03 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Box from "./component/Box";
+
+const choice = {
+  rock: { name: "Rock", img: "/images/hand-rock.svg" },
+  paper: { name: "Paper", img: "/images/hand-paper.svg" },
+  scissors: { name: "Scissors", img: "/images/hand-scissors.svg" },
+};
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="box-containers">
+        <Box title="You" result="User result" item={userSelect} />
+        <Box title="Computer" result="Computer result" />
+      </div>
+      <div>
+        <h1 className="button-title">Choose your move</h1>
+        <div className="rock-paper-scissors-btn">
+          <button onClick={() => play("rock")}>
+            <img src="/images/hand-rock.svg" alt="hand-rock-img"></img>
+            <div>Rock</div>
+          </button>
+          <button onClick={() => play("paper")}>
+            <img src="/images/hand-paper.svg" alt="hand-paper-img"></img>
+            <div>Paper</div>
+          </button>
+          <button onClick={() => play("scissors")}>
+            <img src="/images/hand-scissors.svg" alt="hand-scissors-img"></img>
+            <div>Scissors</div>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/Box.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <h1>{props.title}</h1>
+      <img
+        className="item-img"
+        src={props.item ? props.item.img : "/images/question.svg"}
+        alt="user-image"
+      ></img>
+      <h2>{props.result}</h2>
+    </div>
+  );
+};
+
+export default Box;
